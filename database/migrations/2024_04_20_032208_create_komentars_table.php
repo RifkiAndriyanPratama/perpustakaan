4de971b37bb4b83b4d6723c4b06fc32ddd45df96
RifkiAndriyan Pratama <rifkiandriyanpratama@gmail.com>
2024-04-22 11:23:31 +0700

    first commit

---
+++ database/migrations/2024_04_20_032208_create_komentars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('komentars', function (Blueprint $table) {
+            $table->id('id_komentar');
+            $table->text('id_film');
+            $table->string('author');
+            $table->text('komentar');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('komentars');
+    }
+};
